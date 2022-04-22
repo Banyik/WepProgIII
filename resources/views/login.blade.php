ad91fa9cbc1cfc0a0e3374f0bebdebe86f2e0ead
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-22 21:30:00 +0200

    Simple login site

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.master')
+@section('title', 'Login')
+@section('content')
+    <form action="{{route('verification')}}" method="POST">
+        @csrf
+        WonderPlace<br>
+        <input name="name" placeholder="Username" required><br>
+        <input name="password" placeholder="Password" type="password" required><br>
+        <input value="Login" type="submit">
+    </form>
+@endsection

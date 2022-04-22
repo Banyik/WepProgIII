1fb98be17452cb9c2902187cf10ac3337158c0ec
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-22 23:59:22 +0200

    Edited table

--- database/migrations/2022_04_22_171905_create_posts.php
+++ database/migrations/2022_04_22_171905_create_posts.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->increments('post_id');
+            $table->increments('id');
             $table->integer('user_id');
             $table->string('post_title', 100);
             $table->string('post',1000);

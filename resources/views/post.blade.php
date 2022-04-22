7a5cb364500b31ada38e9d1e6240a0ae088e3b6e
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-22 23:58:56 +0200

    Edited post site

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,32 @@
 @extends('layout.master')
-@section('title','Post')
+@section('title', $id->post_title)
 @section('content')
-    <form action="{{route('postvalidate')}}" method="POST">
-        @csrf
-        <input placeholder="Title of the post" name="post_title">
-        <input placeholder="Content of the post" name="post_content">
-        <input type="submit" value="Submit">
+    @php use App\Models\User; @endphp
+
+    <form action="{{route('home')}}" method="GET">
+        <input value="Back" type="submit">
     </form>
+
+    {{$id->post_title}}<br>
+    Author: {{User::find($id->user_id)->name}}<br>
+    {{$id->post}}<br>
+
+    Comments<br>
+    <form action="{{route('post_comment', $id->id)}}" method="POST">
+        @csrf
+        <input name="comment">
+        <input value="Submit" type="submit">
+    </form>
+
+    @php
+        $record = App\Models\Comment::orderBy('id','desc')->get();
+    @endphp
+    @foreach($record as $item)
+        @if($item->post_id == $id->id)
+            <tr>
+                <td>Author:{{User::find($item->user_id)->name}}</td><br>
+                <td>{{$item->comment_post}}</a></td><br>
+            </tr>
+            @endif
+    @endforeach
 @endsection

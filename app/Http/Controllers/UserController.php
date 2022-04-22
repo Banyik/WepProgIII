cb55dbaaaba353dff69cbb232b9060b335cfb9d8
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-22 21:31:20 +0200

    Simple user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Auth;
+
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function home(){
-        dd(User::find(1)->auth->authentication);
+    public function login_page(){
+        return view('/login');
     }
+    public function home(){
+        $user = User::find(Auth::id());
+        return view('/home', ['user' => $user]);
+    }
+    function verification(Request $request)
+    {
+        $this->validate($request, [
+            'name'   => ['required'],
+            'password'  => ['required','min:3']
+        ]);
+        $user = array(
+            'name' => $request->get('name'),
+            'password' => $request->get('password')
+        );
+        if(Auth::attempt($user))
+        {
+            return redirect('/home');
+        }
+        else
+        {
+            return back()->with('error', 'Wrong Login Details');
+        }
+    }
+    function logout()
+    {
+        Session::flush();
+        Auth::logout();
+        return redirect('/login');
+    }
+
 }

540b1c0ba34c77733fe914d0e4ec69e51becc6a5
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-22 23:59:04 +0200

    Edited home site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,15 @@
 @extends('layout.master')
 @section('title', 'Home')
 @section('content')
-    <form method="GET" action="{{route('post')}}">
+    <form method="GET" action="{{route('create_post')}}">
         <input name="postbutton" type="submit", value="Post">
     </form>
     @php
-        $record = App\Models\Post::orderBy('post_id','desc')->get();
+        $record = App\Models\Post::orderBy('id','desc')->get();
         @endphp
     @foreach($record as $item)
         <tr>
-            <td>{{$item->post_title}}</td><br>
+            <td><a href="{{route('post', $item)}}">{{$item->post_title}}</a></td><br>
         </tr>
     @endforeach
     @if($user->auth->authentication == 9)

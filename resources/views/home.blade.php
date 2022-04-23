b430e95d4f29255c30517bcdbf61872156db53ac
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-23 22:27:51 +0200

    Edited home site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,25 +10,32 @@
         @endphp
 
     <div class="flex flex-1">
-        <div class="flex flex-col flex-auto">
+        <div class="flex flex-col h-[90vh] flex-auto overflow-auto overflow-y-scroll">
             @foreach($record as $item)
-                <div class="backdrop-blur-sm shadow-lg rounded p-2 m-1 bg-white/50 flex flex-col">
+                <div class="backdrop-blur-md shadow-lg rounded p-2 m-1 bg-white/50 flex flex-col ">
 
                     <p class="text-xl">
                         <a
-                            class="hover:text-text-cyan-800 hover:underline" href="{{route('post', $item)}}">
+                            class="hover:text-cyan-900 hover:underline" href="{{route('post', $item)}}">
                             {{$item->post_title}}
                         </a>
                     </p>
 
                     <p class="text-sm">
                         <a
-                            class="hover:text-text-cyan-800 hover:underline" href="{{route('user_site', $item->user_id)}}">
-                            {{$item->user->name}}
-                            {{$item->created_at}}
-                        </a>
+                            class="hover:text-cyan-900 hover:underline" href="{{route('user_site', $item->user_id)}}">
+                            {{$item->user->name}}</a> {{$item->created_at->format('Y-m-d')}}
+                        @if(Auth::check())
+                            @if($item->user->id == Auth::id() || User::find(Auth::id())->auth->authentication == 9)
+                            <a
+                                class="hover:text-cyan-900 hover:underline" href="{{route('home', $item->user_id)}}">
+                                Edit</a>
+                            <a
+                                class="hover:text-cyan-900 hover:underline" href="{{route('home', $item->user_id)}}">
+                                Delete</a>
+                            @endif
+                        @endif
                     </p>
-
                 </div>
             @endforeach
 
@@ -36,23 +43,35 @@
 
         <div class="float-right">
             <div class="bg-white/50 backdrop-blur-sm max-h-full flex grow-0 flex-col shadow-lg rounded p-1 m-1">
-                <a href="{{route('user_site', Auth::id())}}">
-                    <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-cyan-700/70">
-                        <p class="text-xl">{{User::find(Auth::id())->name}}</p>
-                    </div>
-                </a>
-                <a href="{{route('create_post')}}">
-                    <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-cyan-700/70">
-                        <p class="text-xl">Create a post</p>
-                    </div>
-                </a>
-                <a href="{{route('logout')}}">
-                    <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-red-500">
-                        <p class="text-xl">Logout</p>
-                    </div>
-                </a>
+                @if(Auth::check())
+                    <a href="{{route('user_site', Auth::id())}}">
+                        <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-cyan-700/70">
+                            <p class="text-xl">{{User::find(Auth::id())->name}}</p>
+                        </div>
+                    </a>
+                    <a href="{{route('create_post')}}">
+                        <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-cyan-700/70">
+                            <p class="text-xl">Create a post</p>
+                        </div>
+                    </a>
+                    <a href="{{route('logout')}}">
+                        <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-red-500">
+                            <p class="text-xl">Logout</p>
+                        </div>
+                    </a>
+                @endif
+                @if(!Auth::check())
+                        <a href="{{route('login')}}">
+                            <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-cyan-700/70">
+                                <p class="text-xl">Login</p>
+                            </div>
+                        </a>
+                        <a href="{{route('home')}}">
+                            <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-cyan-700/70">
+                                <p class="text-xl">Sign Up</p>
+                            </div>
+                        </a>
+                @endif
             </div>
-
-    @if($user->auth->authentication == 9)
-    @endif
+        </div>
 @endsection

b349932e5969a6745c6fcdc2e0b3d8ede77d4b7c
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-23 22:28:06 +0200

    Edited post create site

--- resources/views/create_post.blade.php
+++ resources/views/create_post.blade.php
@@ -1,10 +1,43 @@
 @extends('layout.master')
 @section('title','Post')
 @section('content')
-    <form action="{{route('post_validate')}}" method="POST">
-        @csrf
-        <input placeholder="Title of the post" name="post_title">
-        <input placeholder="Content of the post" name="post_content">
-        <input type="submit" value="Submit">
-    </form>
+    @php
+        use App\Models\User;
+    @endphp
+    <div class="flex flex-1">
+
+            <div class="float-left flex flex-col flex-auto">
+                <div class="float-left backdrop-blur-sm shadow-lg rounded p-2 m-1 bg-white/50">
+                    <form action="{{route('post_validate')}}" method="POST">
+                        @csrf
+                        <input required class="w-full text-base flex-col backdrop-blur-sm shadow-lg my-1 rounded bg-white/5 focus:outline-none focus:outline-white border-transparent border-2 focus:ring" placeholder="Title of the post" name="post_title">
+                        <textarea required class="w-full h-full text-base resize-y flex flex-col my-1 backdrop-blur-sm shadow-lg rounded bg-white/5 focus:outline-none focus:outline-white border-transparent border-2 focus:ring" placeholder="Content of the post" name="post_content"></textarea>
+                        <input class="text-center rounded p-2 m-1 hover:backdrop-blur-sm hover:text-white/70 hover:bg-cyan-700/70 bg-white/50" type="submit" onclick="this.hidden=true" value="Submit">
+                    </form>
+                </div>
+
+
+            </div>
+
+        <div class="float-right">
+            <div class="bg-white/50 backdrop-blur-sm max-h-full flex grow-0 flex-col shadow-lg rounded p-1 m-1">
+                <a href="{{route('user_site', Auth::id())}}">
+                    <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-cyan-700/70">
+                        <p class="text-xl">{{User::find(Auth::id())->name}}</p>
+                    </div>
+                </a>
+                <a href="{{route('create_post')}}">
+                    <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-cyan-700/70">
+                        <p class="text-xl">Create a post</p>
+                    </div>
+                </a>
+                <a href="{{route('logout')}}">
+                    <div class="text-center rounded p-2 m-1 hover:backdrop-blur-sm  hover:text-white/70 hover:bg-red-500">
+                        <p class="text-xl">Logout</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
 @endsection

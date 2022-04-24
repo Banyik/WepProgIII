71734cc1c0d93dcf26d4ae57eb95e21f372db69d
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-24 13:35:22 +0200

    edited login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,21 +3,31 @@
 @section('content')
     <div class="grid place-items-center  h-[80vh] ">
         <div class="items-center justify-center backdrop-blur-sm shadow-lg rounded p-8 m-1 bg-white/50 ">
-            <form action="{{route('verification')}}" method="POST">
+            <form id="loginForm" name="loginForm" action="{{route('verification')}}" method="POST">
                 @csrf
                 <p class="text-center text-3xl">Login</p>
-                <input class="w-full text-base backdrop-blur-sm shadow-lg p-1 my-1 rounded bg-white/5 focus:outline-none focus:outline-white border-transparent border-2 focus:ring" name="name" placeholder="Username" required><br>
-                <input class="w-full text-base backdrop-blur-sm shadow-lg p-1 my-1 rounded bg-white/5 focus:outline-none focus:outline-white border-transparent border-2 focus:ring" name="password" placeholder="Password" type="password" required><br>
-                <input class="float-left text-center rounded p-2 my-1 hover:backdrop-blur-sm hover:text-white/70 hover:bg-cyan-700/70 bg-white/50" onclick="this.hidden=true" value="Login" type="submit">
+                <input id="name" class="w-full text-base backdrop-blur-sm shadow-lg p-1 my-1 rounded bg-white/5 focus:outline-none focus:outline-white border-transparent border-2 focus:ring" name="name" placeholder="Username" required><br>
+                <input id="password" class="w-full text-base backdrop-blur-sm shadow-lg p-1 my-1 rounded bg-white/5 focus:outline-none focus:outline-white border-transparent border-2 focus:ring" name="password" placeholder="Password" type="password" required><br>
+                <input class="float-left text-center rounded p-2 my-1 hover:backdrop-blur-sm hover:text-white/70 hover:bg-cyan-700/70 bg-white/50" onclick="preventMultipleSubmit()" name="submit" type="submit" value="Login">
             </form>
 
             <form action="{{route('register')}}" method="GET">
 
-                <input class="float-right text-center rounded p-2 my-1 hover:backdrop-blur-sm hover:text-white/70 hover:bg-cyan-700/70 bg-white/50" onclick="this.hidden=true" value="Register" type="submit">
+                <input class="float-right text-center rounded p-2 -my-3 hover:backdrop-blur-sm hover:text-white/70 hover:bg-cyan-700/70 bg-white/50" onclick="this.hidden=true" value="Register" type="submit">
             </form>
-            <p class="text-center m-1 py-2">Or</p>
+            <p class="text-center -my-3 py-2">Or</p>
         </div>
 
     </div>
 
 @endsection
+<script>
+    function preventMultipleSubmit(){
+        var username = document.forms['loginForm']['name'].value;
+        var password = document.forms['loginForm']['password'].value;
+        var button = document.forms['loginForm']['submit'];
+        if(username !== "" && password !== ""){
+            button.hidden = true;
+        }
+    }
+</script>
